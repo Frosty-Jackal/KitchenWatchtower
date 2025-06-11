2c7e1e76a6ffeb9329019e838e46216bbbb178ed
FrostyJ <1733799356@qq.com>
2025-06-11 17:05:21 +0800

    add some docs and update foodlic

--- KitchenWatchtower.sql
+++ KitchenWatchtower.sql
@@ -1,24 +1,50 @@
-/*
-Navicat MySQL Data Transfer
+-- MySQL dump 10.13  Distrib 8.3.0, for Win64 (x86_64)
+--
+-- Host: localhost    Database: kitchenwatchtower
+-- ------------------------------------------------------
+-- Server version	8.3.0
 
-Source Server         : localhost
-Source Server Version : 80042
-Source Host           : localhost:3306
-Source Database       : KitchenWatchtower
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-Target Server Type    : MYSQL
-Target Server Version : 80042
-File Encoding         : 65001
+--
+-- Table structure for table `foodlic`
+--
 
-Date: 2025-06-05 16:45:17
-*/
+DROP TABLE IF EXISTS `foodlic`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `foodlic` (
+  `lic_num` varchar(50) DEFAULT NULL,
+  `opt_name` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-SET FOREIGN_KEY_CHECKS=0;
+--
+-- Dumping data for table `foodlic`
+--
 
--- ----------------------------
--- Table structure for liclic
--- ----------------------------
+LOCK TABLES `foodlic` WRITE;
+/*!40000 ALTER TABLE `foodlic` DISABLE KEYS */;
+INSERT INTO `foodlic` VALUES ('hr666','hr233'),('123','123123'),('JJY24407030096311','广东优赢膳食管理有限公司');
+/*!40000 ALTER TABLE `foodlic` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `liclic`
+--
+
 DROP TABLE IF EXISTS `liclic`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `liclic` (
   `lic_id` int DEFAULT NULL,
   `lic_name` varchar(255) DEFAULT NULL,
@@ -26,18 +52,25 @@ CREATE TABLE `liclic` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of liclic
--- ----------------------------
-INSERT INTO `liclic` VALUES ('12', '214', '2025-06-03', '5');
-INSERT INTO `liclic` VALUES ('1234234', '唐彪', '43', '10');
-INSERT INTO `liclic` VALUES ('232', '21', '2020-01-01', '11');
+--
+-- Dumping data for table `liclic`
+--
 
--- ----------------------------
--- Table structure for user
--- ----------------------------
+LOCK TABLES `liclic` WRITE;
+/*!40000 ALTER TABLE `liclic` DISABLE KEYS */;
+INSERT INTO `liclic` VALUES (12,'214','2025-06-03',5),(1234234,'okey','43',10),(232,'21','2020-01-01',11);
+/*!40000 ALTER TABLE `liclic` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
 DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
   `id` int NOT NULL AUTO_INCREMENT,
   `user_name` varchar(255) DEFAULT NULL,
@@ -46,18 +79,25 @@ CREATE TABLE `user` (
   `create_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of user
--- ----------------------------
-INSERT INTO `user` VALUES ('1', '唐彪', '18228268719', '2023141460318', '2025-06-03 21:26:54');
-INSERT INTO `user` VALUES ('3', '3', '18140481158', '21321321321321321321', '2025-06-03 23:23:26');
-INSERT INTO `user` VALUES ('4', '李江', '15984226823', '213213213213213213', '2025-06-03 23:24:28');
+--
+-- Dumping data for table `user`
+--
 
--- ----------------------------
--- Table structure for warehouse
--- ----------------------------
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'jeff','18228268719','2023141460318','2025-06-03 21:26:54'),(3,'3','18140481158','21321321321321321321','2025-06-03 23:23:26'),(4,'鏉庢睙','15984226823','213213213213213213','2025-06-03 23:24:28');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `warehouse`
+--
+
 DROP TABLE IF EXISTS `warehouse`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `warehouse` (
   `id` int NOT NULL AUTO_INCREMENT,
   `food_name` varchar(255) DEFAULT NULL,
@@ -70,9 +110,25 @@ CREATE TABLE `warehouse` (
   `supplier` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of warehouse
--- ----------------------------
-INSERT INTO `warehouse` VALUES ('21', '梨子', '水果', '112', 'oneday', '2025-06-03 20:43:43', 'good', '冷藏', '唐彪');
-INSERT INTO `warehouse` VALUES ('22', '胡萝', 'Vegetables', '143', 'onemonth', '2025-06-04 10:29:20', 'best', '冷藏', 'hr');
+--
+-- Dumping data for table `warehouse`
+--
+
+LOCK TABLES `warehouse` WRITE;
+/*!40000 ALTER TABLE `warehouse` DISABLE KEYS */;
+INSERT INTO `warehouse` VALUES (21,'jessie','12','109','oneday','2025-06-03 20:43:43','good','123','123123'),(22,'22','Vegetables','143','onemonth','2025-06-04 10:29:20','best','no','hr');
+/*!40000 ALTER TABLE `warehouse` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-06-11 16:58:13
